4b87990e411bfd9bdfc9b64fa9ef540929a76f46
SatokoTempest <satokotempest@gmail.com>
2023-06-12 00:00:53 +0330

    Add files via upload

---
+++ Adult_age.py
@@ -0,0 +1,14 @@
+def is_adult():
+  age = int(input("Enter your age :"))
+  if age >= 18 :
+    return "you are an Adult"
+
+  else :
+    return "You are not an Adult"
+
+
+
+
+
+print(is_adult())
+    
